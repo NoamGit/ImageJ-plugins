147981b42f1cee686f048697f61a95b38f6256dc
unknown <noambox@Shy-Yoga.bm.technion.ac.il>
2015-08-27 17:31:34 +0300

    Tom for Noam

---
+++ src/test/java/Activity_AnalysisTest.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by noambox on 8/27/2015.
+ */
+public class Activity_AnalysisTest {
+
+    @org.junit.Test
+    public void testBlobfeature() throws Exception {
+
+    }
+}
